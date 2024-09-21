9a89d8e286e6c63b237efad1afc7eea066c03849
Dennis Gustavsson <58669253+dennisGustavsson@users.noreply.github.com>
2024-09-21 22:10:27 +0200

    updated styling and githublist

---
+++ src/assets/githubprojects.js
@@ -0,0 +1,22 @@
+//make an array of objects with the following properties: name, description, url
+export const githubProjects = [
+  {
+    id: 1,
+    name: "Speech To Text App",
+    description:
+      "A React app that uses the Azure Cognitive Speech Services AI to transcribe speech to text.",
+    url: "https://github.com/dennisGustavsson/reactspeech",
+  },
+  {
+    id: 2,
+    name: "E-commerce Web App using React",
+    description: "School project for a React web app using TypeScript.",
+    url: "https://github.com/dennisGustavsson/BackEndProjectWebappTS",
+  },
+  {
+    id: 3,
+    name: "E-commerce Web App using ASP.NET",
+    description: "An ASP.NET web app for a school project.",
+    url: "https://github.com/dennisGustavsson/ASPNET_Assignment_DG",
+  },
+];
--- src/components/GitHubProjectsComponent.jsx
+++ src/components/GitHubProjectsComponent.jsx
@@ -1,40 +1,67 @@
 import { useEffect, useState } from "react";
+import { githubProjects } from "../assets/githubprojects";
+import { q } from "framer-motion/client";
 
 const GitHubProjectsComponent = () => {
-  const specificRepos = [
-    "BackEndProjectWebappTS",
-    "ASPNET_Assignment_DG",
-    "CMS_Assignment_DG",
-  ];
+  // const specificRepos = [
+  //   "reactspeech",
+  //   "BackEndProjectWebappTS",
+  //   "ASPNET_Assignment_DG",
+  //   "CMS_Assignment_DG",
+  // ];
 
-  const [projects, setProjects] = useState([]);
-  useEffect(() => {
-    const fetchRepos = async () => {
-      try {
-        const repoPromises = specificRepos.map((repo) =>
-          fetch(`https://api.github.com/repos/dennisGustavsson/${repo}`).then(
-            (response) => response.json()
-          )
-        );
+  const [projects, setProjects] = useState(githubProjects);
+  const [selectedProjectId, setSelectedProjectId] = useState(null);
 
-        const repoData = await Promise.all(repoPromises);
-        setProjects(repoData);
-      } catch (error) {
-        console.error("Error fetching GitHub repos:", error);
-      }
-    };
+  const handleProjectClick = (projectId) => {
+    setSelectedProjectId(selectedProjectId === projectId ? null : projectId);
+  };
 
-    fetchRepos();
-  }, []);
+  // useEffect(() => {
+  //   const fetchRepos = async () => {
+  //     try {
+  //       const repoPromises = specificRepos.map((repo) =>
+  //         fetch(`https://api.github.com/repos/dennisGustavsson/${repo}`).then(
+  //           (response) => response.json()
+  //         )
+  //       );
+
+  //       const repoData = await Promise.all(repoPromises);
+  //       setProjects(repoData);
+  //     } catch (error) {
+  //       console.error("Error fetching GitHub repos:", error);
+  //     }
+  //   };
+
+  //   fetchRepos();
+  // }, []);
 
   return (
     <>
       <ul className="gh-list">
         {projects.map((project) => (
           <li key={project.id}>
-            <a href={project.html_url} target="_blank">
+            <a
+              href={project.url}
+              target="_blank"
+              rel="noopener noreferrer"
+              onClick={(e) => {
+                e.preventDefault(); // Prevent the default link behavior
+                handleProjectClick(project.id);
+              }}
+            >
               {project.name}
             </a>
+            {selectedProjectId === project.id && (
+              <>
+                <div className="description">
+                  {project.description}
+                  <a className="ghBtn" href={project.url} target="_blank">
+                    <i className="fa-brands fa-github"></i>
+                  </a>
+                </div>
+              </>
+            )}
           </li>
         ))}
       </ul>
