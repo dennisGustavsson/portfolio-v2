51578498b6b857a7169965097970b5e0e6274566
Dennis Gustavsson <58669253+dennisGustavsson@users.noreply.github.com>
2024-10-06 14:57:44 +0200

    added animation to contact modal

--- src/components/ModalComponent.jsx
+++ src/components/ModalComponent.jsx
@@ -1,39 +1,63 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import PropTypes from "prop-types";
 
 const ModalComponent = ({ shouldShow, onRequestClose, children }) => {
+  const [isClosing, setIsClosing] = useState(false);
+
+  const handleClose = () => {
+    setIsClosing(true);
+    setTimeout(() => {
+      setIsClosing(false);
+      onRequestClose();
+    }, 300);
+  };
+
   useEffect(() => {
     const handleEscape = (event) => {
       if (event.key === "Escape") {
-        onRequestClose();
+        handleClose();
       }
     };
+
+    const handleClickOutside = (event) => {
+      if (event.target.classList.contains("modal-background")) {
+        handleClose();
+      }
+    };
+
     document.addEventListener("keydown", handleEscape);
+    document.addEventListener("mousedown", handleClickOutside);
+
     return () => {
       document.removeEventListener("keydown", handleEscape);
+      document.removeEventListener("mousedown", handleClickOutside);
     };
   }, [onRequestClose]);
 
-  if (!shouldShow) {
+  if (!shouldShow && !isClosing) {
     return null;
   }
 
   return (
     <div
-      className="modal-background"
-      onClick={onRequestClose}
+      className={`modal-background ${
+        isClosing ? "modal-bg-out" : "modal-bg-in"
+      }`}
+      onClick={handleClose}
       role="dialog"
       aria-modal="true"
     >
       <div
-        className="modal-content"
+        className={`modal-content ${
+          isClosing ? "modal-close" : "modal-appear"
+        }`}
         onClick={(e) => {
           e.stopPropagation();
         }}
       >
         <button
           className="modal-close-button"
-          onClick={onRequestClose}
+          onClick={handleClose}
           aria-label="Close modal"
         >
           &times;
